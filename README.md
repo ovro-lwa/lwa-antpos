9b2e6daa9cdd3620f7d2c4b0f559a42bfce63b2d
Casey Law <caseyjlaw@gmail.com>
2021-03-16 14:26:41 -0700

    working station ovrolwa. readme updated

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # lwa-antpos
+
+# Requirements
+* pandas
+* numpy
+* astropy
+* dsa110-pyutils
+
+# Installation
+
+`python setup.py install`
+
+# Usage
+```
+> from lwa_antpos import station
+> st = station.ovrolwa
+> print(st)
+Station LWA-000 with 352 antennas
+> print(st.casa_position)
+('ITRF', '-2409261.733561m', '-4477916.567884m', '3839351.138694m')
+> st2 = st.select_subset([0, 1, 2])
+> print(st2.antennas)
+[Antenna 1 in Station OVRO-LWA with 352 antennas,
+ Antenna 2 in Station OVRO-LWA with 352 antennas,
+ Antenna 3 in Station OVRO-LWA with 352 antennas]
+> ant = st.antennas[0]
+> print(ant.lat, ant.lon)
+0.6499685035526378, -2.064391274631196
+```
--- lwa_antpos/config.py
+++ lwa_antpos/config.py
@@ -2,18 +2,20 @@ import dsautils.cnf as cnf
 from pkg_resources import resource_filename
 from . import reading
 
+__all__ = ['lwa_cnf']
+
 CNFCONF = resource_filename("lwa_antpos", "data/cnfConfig.yml")
 # TODO: eventually my_cnf can be read from etcd here
 df = reading.read_antpos_xlsx()
 dd = {'ant': df.set_index('Name'), 'fpga': df.set_index('FPGA')}
-my_cnf = cnf.Conf(data=dd, cnf_conf=CNFCONF)
+lwa_cnf = cnf.Conf(data=dd, cnf_conf=CNFCONF)
 
 
 def antenna(name):
     """ Get metadata for one antenna from datafram.
     """
 
-    df_ant = my_cnf.get('ant')
+    df_ant = lwa_cnf.get('ant')
     
     return df_ant.loc[name]
 
@@ -24,7 +26,7 @@ def baseline(a1, a2):
     TODO: add argument to select antennas
     """
 
-    df_ant = my_cnf.get('ant')
+    df_ant = lwa_cnf.get('ant')
     
     d1 = df_ant.loc[a1]
     d2 = df_ant.loc[a2]
@@ -35,7 +37,7 @@ def fpga(number):
     """ Get metadata for an FPGA as dataframe.
     """
 
-    df_fpga = my_cnf.get('fpga')
+    df_fpga = lwa_cnf.get('fpga')
 
     return df_fpga.loc[number]
 
