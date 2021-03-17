61a4813c887b179ff1a0c77b58b9036abe39519a
Casey Law <caseyjlaw@gmail.com>
2021-03-17 12:03:09 -0700

    fixing dependency setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='lwa-antpos',
       version=get_git_version(),
       url='http://github.com/ovro-lwa/lwa-antpos/',
       packages=['lwa_antpos'],
-      requirements=['openpyxl', 'pandas', 'numpy'],
+      install_requires=['openpyxl', 'pandas>=0.2.5', 'numpy'],
       package_data={
           'lwa_antpos': ['data/LWA-352*xlsx', 'data/*yml']},
       zip_safe=False)
