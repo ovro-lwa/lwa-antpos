cc07613b115ac069d07788de14fd2cdc6fdf45da
Casey Law <caseyjlaw@gmail.com>
2021-06-28 09:49:10 -0700

    reorg to remove config.py

--- lwa_antpos/__init__.py
+++ lwa_antpos/__init__.py
@@ -1,1 +1,14 @@
-__all__ = ['reading', 'config']
+__all__ = ['reading', 'station', 'lwa_cnf', 'lwa_df']
+
+import dsautils.cnf as cnf
+from pkg_resources import resource_filename
+from . import reading
+
+CNFCONF = resource_filename("lwa_antpos", "data/cnfConfig.yml")
+# TODO: eventually my_cnf can be read from etcd here
+lwa_df = reading.read_antpos_xlsx()
+dds = {'ant': {}}
+for ind in lwa_df.index:
+    dd = lwa_df.loc[ind]
+    dds['ant'][ind] = dd
+lwa_cnf = cnf.Conf(data=dds, cnf_conf=CNFCONF)
