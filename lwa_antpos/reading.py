013b52839add8f1c4a63c0d657ac39133cd0edef
Casey Law <caseyjlaw@gmail.com>
2021-08-07 08:13:47 -0700

    parsing etcd

--- lwa_antpos/reading.py
+++ lwa_antpos/reading.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import yaml
+import json
 from pkg_resources import resource_filename
 
 antposfile = resource_filename("lwa_antpos", "data/LWA-352 Antenna Status & System Configuration.xlsx")
@@ -14,15 +15,23 @@ def read_antpos_xlsx(filename=antposfile):
     df.drop(index=0, inplace=True)  # descriptive header should be dropped
     df.drop(columns='dict_key', inplace=True)  # descriptive column should be dropped
     assert "antname" in df.columns
-    df = df.set_index('antname')
+    df.set_index('antname', inplace=True)
 
     return df
 
-def read_antpos_etcd(host, port):
+
+def read_antpos_etcd(host='10.42.0.64', port=2379):
     """ Gets data from etcd and returns dataframe
     """
 
-    raise
+    dds, _ = le.get('/cfg/system') 
+    dd = json.loads(dd.decode("utf-8"))['lwacfg']
+    
+    df = pd.DataFrame.from_dict(dd)
+    assert "antname" in df.columns
+    df.set_index('antname', inplace=True)
+
+    return df
 
 
 def read_antpos_yaml(filename):
